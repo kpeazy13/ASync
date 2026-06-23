1262046e0e7a2054a0fbf47697363e83f91264e5
Krish Patel <patelkrishc5@gmail.com>
2026-06-23 17:35:28 -0500

    refactor parsing into parse function

--- parser.py
+++ parser.py
@@ -1,13 +1,18 @@
 import icalendar
 from pathlib import Path
 
-ics_path = Path("C:\\Users\\patel\\Downloads\\ASync (py)\\ASync\\user_z9rQL7Ko05ms2NrNU42RWzQyZ0w8fkX4SD6CbNBV.ics")
-calendar = icalendar.Calendar.from_ical(ics_path)
-for event in calendar.events:
-    uid = str(event.get('UID'))
-    if "assignment" in uid.lower():
-        print(f"Event UID: {uid}")
-        print(f"Summary: {event.get('SUMMARY')}")
-        print(f"Start Time: {event.get('DTSTART').dt}")
-        print(f"Description: {event.get('DESCRIPTION')}")
-        print("-" * 40)
+def parse_ics_file(ics_file_path):    
+    calendar = icalendar.Calendar.from_ical(ics_file_path)
+    assignment_list = []
+    for event in calendar.events:
+        assignment = {}
+        uid = str(event.get('UID'))
+        if "assignment" in uid.lower():
+            assignment['uid'] = uid
+            assignment['summary'] = str(event.get('SUMMARY'))
+            assignment['description'] = str(event.get('DESCRIPTION'))
+            assignment['due'] = event.get('DTSTART').dt
+            assignment_list.append(assignment)
+    return assignment_list
+
+print(parse_ics_file(Path("C:\\Users\\patel\\Downloads\\ASync (py)\\ASync\\user_z9rQL7Ko05ms2NrNU42RWzQyZ0w8fkX4SD6CbNBV.ics")))
