59cdc1e79a1ac971c68ceae564c903abf183d7cf
Krish Patel <patelkrishc5@gmail.com>
2026-06-23 17:24:58 -0500

    Parsed ICS feed and filtered only for assignments

--- parser.py
+++ parser.py
@@ -4,6 +4,10 @@ from pathlib import Path
 ics_path = Path("C:\\Users\\patel\\Downloads\\ASync (py)\\ASync\\user_z9rQL7Ko05ms2NrNU42RWzQyZ0w8fkX4SD6CbNBV.ics")
 calendar = icalendar.Calendar.from_ical(ics_path)
 for event in calendar.events:
-    print(event.get("SUMMARY"))
-    print(event.get("DTSTART").dt)
-    print(event.get("UID"))
+    uid = str(event.get('UID'))
+    if "assignment" in uid.lower():
+        print(f"Event UID: {uid}")
+        print(f"Summary: {event.get('SUMMARY')}")
+        print(f"Start Time: {event.get('DTSTART').dt}")
+        print(f"Description: {event.get('DESCRIPTION')}")
+        print("-" * 40)
